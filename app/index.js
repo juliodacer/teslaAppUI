9cd6d3f9ba3fd798611ee2f7513c5f005e6de741
Julio Davila <juliodacer@gmail.com>
2023-04-02 19:17:50 -0500

    flatList MenuOption with icons

--- app/index.js
+++ app/index.js
@@ -1,16 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
-import { FontAwesome } from '@expo/vector-icons'
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import {
+  FontAwesome,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  FontAwesome5,
+  Ionicons,
+  Entypo,
+} from "@expo/vector-icons";
+import car from "../assets/images/car.png";
+import menuOptions from "../assets/menuOptions";
+import MenuOption from "../components/MenuOption";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-       <View>
-        <Text style={styles.title}>My model S</Text>
-        <Text style={styles.subtitle}>Parked</Text>
-       </View>
-      <FontAwesome name="user-circle" size={30} color="gray"/>
+        <View>
+          <Text style={styles.title}>My model S</Text>
+          <Text style={styles.subtitle}>Parked</Text>
+        </View>
+        <FontAwesome name="user-circle" size={30} color="gray" />
       </View>
+      <Image source={car} style={styles.image} resizeMode="contain" />
+
+      {/* Icons flex directions */}
+      <View style={styles.controls}>
+        <Entypo name="lock" size={26} color="gray" />
+        <MaterialCommunityIcons name="fan" size={26} color="gray" />
+        <FontAwesome5 name="bolt" size={26} color="gray" />
+        <Ionicons name="car-sport-sharp" size={26} color="gray" />
+      </View>
+
+      {/* Menu Options Item */}
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={menuOptions}
+        renderItem={MenuOption}
+      />
     </View>
   );
 }
@@ -20,21 +46,30 @@ const styles = StyleSheet.create({
     flex: 1,
     // alignItems: "center",
     padding: 24,
-    backgroundColor: '#161818'  //tesla color
+    backgroundColor: "#161818", //tesla color
   },
   header: {
     marginTop: 20,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 24,
-    color: '#eee',
+    color: "#eee",
     fontWeight: "bold",
-    marginBottom: 8
+    marginBottom: 8,
   },
   subtitle: {
     color: "gray",
-    fontWeight: '500'
+    fontWeight: "500",
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  controls: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 20,
   },
 });
